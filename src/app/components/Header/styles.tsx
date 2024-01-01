91e19bb82ee69692a352448e98b83454d238f01b
brits <joseluizbrits@gmail.com>
2024-01-01 20:45:21 -0300

    style: add shadow to Header

--- src/app/components/Header/styles.tsx
+++ src/app/components/Header/styles.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.header`
   background-color: var(--primary);
   padding: 20px 64px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);
 
   display: flex;
   justify-content: space-between;
